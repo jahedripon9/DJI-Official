d738cf61814d0ce82767ab425ba9fc310a389a6f
jahedripon9 <jahedahmed3170@gmail.com>
2021-11-12 08:35:47 +0600

    editing

--- src/Pages/Login/Firebase/firebase.config.js
+++ src/Pages/Login/Firebase/firebase.config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyB6BETjq9yxazJvZA0eOzKJRd1kvjN8REs",
-  authDomain: "dji-official-website.firebaseapp.com",
-  projectId: "dji-official-website",
-  storageBucket: "dji-official-website.appspot.com",
-  messagingSenderId: "618780590520",
-  appId: "1:618780590520:web:27abe2516cc8c6b7b60f4d"
-};
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  };
 
   export default firebaseConfig;
